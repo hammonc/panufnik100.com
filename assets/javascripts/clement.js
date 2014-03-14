ad059ea3f535ac6cf9343196dbf671e16d2114df
Peter Inglesby <peter.inglesby@gmail.com>
2014-03-14 00:47:22 +0000

    Use submarine for contact form

--- assets/javascripts/clement.js
+++ assets/javascripts/clement.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
 	$(".fancybox").fancybox();
 	$(".popupwindow").popupwindow();
+
+	$("form#mailing-list").validate({
+		rules: {
+			name: {
+				required: false,
+			},
+			email: {
+				required: true,
+				email: true
+			},
+		}
+	});
 });
